fc0d87467ff8241bb757c825db9ba9176aba9a3f
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2024-01-25 17:10:43 -0300

    Initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Harvard CS50 Exercises (Games)
-
-My exercises made for my harvard CS50 course in 2024.
-
-Please, check the other branches, each one represents one exercise (game). I hope you like it.
---
+++ src/Tile.lua
@@ -0,0 +1,56 @@
+--[[
+    GD50
+    Match-3 Remake
+
+    -- Tile Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The individual tiles that make up our game board. Each Tile can have a
+    color and a variety, with the varietes adding extra points to the matches.
+]]
+
+Tile = Class{}
+
+function Tile:init(x, y, color, variety, shiny)
+    
+    -- board positions
+    self.gridX = x
+    self.gridY = y
+
+    -- coordinate positions
+    self.x = (self.gridX - 1) * 32
+    self.y = (self.gridY - 1) * 32
+
+    -- tile appearance/points
+    self.color = color
+    self.variety = variety
+    self.shiny = shiny
+end
+
+function Tile:render(x, y)
+    
+    -- draw shadow
+    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x + 2, self.y + y + 2)
+
+    -- draw tile itself
+    love.graphics.setColor(255, 255, 255, 255)    
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x, self.y + y)
+      
+    if self.shiny then
+        -- multiply so drawing white rect makes it brighter
+        love.graphics.setBlendMode('add')
+
+        love.graphics.setColor(1, 1, 1, 96/255)
+        love.graphics.rectangle('fill', self.x + x,
+            self.y + y, 32, 32, 4)
+
+        -- back to alpha
+        love.graphics.setBlendMode('alpha')
+    else  
+    end
+end
